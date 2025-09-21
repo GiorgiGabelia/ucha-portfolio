69b8d75b2f1a89cb067c802d96c438ca12a66abe
GiorgiGabelia <giorgi.gabelia321@gmail.com>
2025-09-21 13:56:41 +0400

    refactor carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,13 +1,28 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 
 @Component({
   selector: 'app-carousel',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
 })
 export class CarouselComponent {
-  readonly images = Array.from({ length: 7 }, (_, i) => `assets/${i + 1}.png`)
+  readonly images = Array.from({ length: 7 }, (_, i) => `assets/${i + 1}.png`);
+
+  hoveredImgIndexInSecondUl = signal<null | number>(null);
+
+  secondUlMargin = computed(() => {
+    const hoveredImgIndex = this.hoveredImgIndexInSecondUl();
+    const margin = hoveredImgIndex ? (hoveredImgIndex + 1) * 30 : 0;
+    return margin + 'px';
+  })
+
+  onMouseEnter(index: number) {
+    this.hoveredImgIndexInSecondUl.set(index);
+  }
+
+  onMouseLeave() {
+    this.hoveredImgIndexInSecondUl.set(null);
+  }
 }
